a571d15afbd1131e863d88005dfb6989af70051b
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-09-20 11:37:31 -0600

    finished stretch goals

--- challenges/classes.js
+++ challenges/classes.js
@@ -14,7 +14,24 @@ class CuboidMakers {
     }
 }
 
-
+//stretch goal
+class CubeMaker extends CuboidMaker{
+    constructor(att){
+        super(att)
+    }
+    cubeVol(){
+        return (this.length * this.height * this.width);
+    }
+    cubeSa(){
+        return 2*(this.length * this.width)+ 2*(this.length * this.height) + 2*(this.width * this.height);
+    }
+}
+const cube = new CubeMaker({
+    length: 4,
+    width: 4,
+    height:4
+})
+// end stretch goal
 const cuboids = new CuboidMakers({
     length: 4,
     width: 5,
@@ -24,4 +41,7 @@ const cuboids = new CuboidMakers({
 console.log(cuboids.volume()); // 100
 console.log(cuboids.sa()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes an
+console.log(cube.cubeSa());
+console.log(cube.cubeVol())
+
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
